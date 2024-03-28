8ffb570876e473d78941d35ef0643f8d3d3ee7df
Your Name <you@example.com>
2024-03-28 09:39:49 +0200

    adedd hello

--- main.py
+++ main.py
@@ -14,10 +14,17 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 
 
+async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Дарова чел.")
+
+
 if __name__ == '__main__':
     application = ApplicationBuilder().token(my_telegram_bot).build()
 
     start_handler = CommandHandler('start', start)
     application.add_handler(start_handler)
+
+    hello_handler = CommandHandler('hello', hello)
+    application.add_handler(hello_handler)
 
     application.run_polling()
